129b4d1e5eb855f100ed2924f98422be9165bf0c
Qi Chao Ma <skye1044@gmail.com>
2022-03-21 02:10:14 +0800

    in the midst of implementing gossip protocol

--- src/communication.go
+++ src/communication.go
@@ -1,5 +1,58 @@
 package main
 
-func hi() {
+import (
+	"os"
+	"strings"
+)
 
+const (
+	CONN_PORT = ":8080"
+	CONN_TYPE = "tcp"
+)
+
+type node struct {
+	membership    bool
+	containerName string // for network comm
+	// nodeID
+	// tokenSet
+	// timeOfIssue int
+
+}
+
+func start(done chan<- struct{}) {
+	nodeMap := make(map[string]node)
+}
+
+func connect(containerName string) {
+
+}
+
+func getContainerName() string {
+	var output string
+	switch os.Getenv("NODE_ID") {
+	case "0":
+		output = "node0"
+	case "1":
+		output = "node1"
+	case "2":
+		output = "node3"
+	}
+	return output
+}
+
+func getSeedNodes() []string {
+	return strings.Split(os.Getenv("SEEDNODES"), " ")
+}
+
+func nodeidToContainerName(nodeid string) string {
+	var containerName string
+	switch nodeid {
+	case "0":
+		containerName = "node0"
+	case "1":
+		containerName = "node1"
+	case "2":
+		containerName = "node2"
+	}
+	return containerName
 }
--- src/main.go
+++ src/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	s "strings"
 	"time"
 )
 
 func main() {
+	// done := make(chan struct{})
+
 	for {
+		seedNodes := os.Getenv("SEEDNODES")
+		seednodes := s.Split(seedNodes, " ")
+		containerName := getContainerName()
 		fmt.Println("NODE ID:", os.Getenv("NODE_ID"))
+		fmt.Println("Membership is", os.Getenv("MEMBERSHIP"))
+		fmt.Println("seedNodes =", seednodes[0], seednodes[1])
+		fmt.Println("my container name is", containerName)
 		fmt.Println("New")
 		time.Sleep(time.Second * 10)
 	}
